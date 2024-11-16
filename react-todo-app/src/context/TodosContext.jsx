fa2888ae74d82d88562bbb924b32234af569d28f
Mwan3kii <agathawanja303@gmail.com>
2024-11-16 19:47:29 +0300

    Create TodosContext for global state management of to-do items

---
+++ react-todo-app/src/context/TodosContext.jsx
@@ -0,0 +1,27 @@
+import {
+  useState,
+  useEffect,
+  createContext,
+  useContext,
+} from 'react';
+
+import { v4 as uuidv4 } from 'uuid';
+
+const TodosContext = createContext(null);
+
+export const TodosProvider = ({ children }) => {
+  return (
+    <TodosContext.Provider
+    value={{
+      todos,
+      handleChange,
+      delTodo,
+      addTodoItem,
+      setUpdate,
+    }}
+    >
+      {children}
+    </TodosContext.Provider>
+  );
+};
+export const useTodosContext = () => useContext(TodosContext);
